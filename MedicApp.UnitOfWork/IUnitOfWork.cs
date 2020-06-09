1f3574aad409e20e6acb23aeccf5585030c501bf
Pablo Ismael Sanchez Rijo <pablo17sanchez@hotmail.com>
2020-06-09 13:07:08 -0400

    Commenting the unitofworkclass

--- MedicApp.UnitOfWork/IUnitOfWork.cs
+++ MedicApp.UnitOfWork/IUnitOfWork.cs
@@ -2,8 +2,19 @@
 
 namespace MedicApp.UnitOfWork
 {
+
+//Esta es una interface que contiene las interfaces de los respositorios que utilizo para modificar mi estructura
+//Esto se usa por un patron llamado UNItOfwork 
+
+/*En primera instancia UoW soluciona un problema sobre las conexiones a la base de datos y
+permite encapsular dichas consultas a la
+DB en una sola pudiendo así hacer varias tareas y al final realizar los cambios.*/
+
+//Mas referencias https://anexsoft.com/ejemplo-de-unit-of-work-con-c
+
     public interface IUnitOfWork
     {
+        
         IMedicoRepository Medico { get; }
         ISectorRepository Sector { get; }
         IConsultoriosRepository Consultorio { get; }
